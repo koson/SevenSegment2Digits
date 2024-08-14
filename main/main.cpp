c9a164f33b6e3fd63d34659af4be844e6eef7809
koson <567256+koson@users.noreply.github.com>
2024-08-15 00:58:36 +0700

    update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -2,55 +2,20 @@
 
 #include "SevenSegment.h"
 
-SevenSegment s1(0) ;
-SevenSegment s2(4) ;
+SevenSegment s1(0);
+SevenSegment s2(4);
 
 extern "C" void app_main(void)
 {
-while(1)
+    while (1)
     {
-        s1.DisplayNum0();
-        vTaskDelay(500/portTICK_PERIOD_MS);
-        s1.DisplayNum1();
-        vTaskDelay(500/portTICK_PERIOD_MS);
-        s1.DisplayNum2();
-        vTaskDelay(500/portTICK_PERIOD_MS);
-        s1.DisplayNum3();
-        vTaskDelay(500/portTICK_PERIOD_MS);
-        s1.DisplayNum4();
-        vTaskDelay(500/portTICK_PERIOD_MS);
-        s1.DisplayNum5();
-        vTaskDelay(500/portTICK_PERIOD_MS);
-        s1.DisplayNum6();
-        vTaskDelay(500/portTICK_PERIOD_MS);
-        s1.DisplayNum7();
-        vTaskDelay(500/portTICK_PERIOD_MS);
-        s1.DisplayNum8();
-        vTaskDelay(500/portTICK_PERIOD_MS);
-        s1.DisplayNum9();
-        vTaskDelay(500/portTICK_PERIOD_MS);
-        s1.DisplayBlank();
+        for (int i = 0; i < 9; i++)
+        {
+            s1.DisplayNumber(i);
+            vTaskDelay(500 / portTICK_PERIOD_MS);
 
-        s2.DisplayNum0();
-        vTaskDelay(500/portTICK_PERIOD_MS);
-        s2.DisplayNum1();
-        vTaskDelay(500/portTICK_PERIOD_MS);
-        s2.DisplayNum2();
-        vTaskDelay(500/portTICK_PERIOD_MS);
-        s2.DisplayNum3();
-        vTaskDelay(500/portTICK_PERIOD_MS);
-        s2.DisplayNum4();
-        vTaskDelay(500/portTICK_PERIOD_MS);
-        s2.DisplayNum5();
-        vTaskDelay(500/portTICK_PERIOD_MS);
-        s2.DisplayNum6();
-        vTaskDelay(500/portTICK_PERIOD_MS);
-        s2.DisplayNum7();
-        vTaskDelay(500/portTICK_PERIOD_MS);
-        s2.DisplayNum8();
-        vTaskDelay(500/portTICK_PERIOD_MS);
-        s2.DisplayNum9();
-        vTaskDelay(500/portTICK_PERIOD_MS);
-        s2.DisplayBlank();
-    } 
+            s2.DisplayNumber(i);
+            vTaskDelay(500 / portTICK_PERIOD_MS);
+        }
+    }
 }
